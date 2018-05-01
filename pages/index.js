01e60f9fbc3ece89aa38200f8e5d2c2cb3121ca3
Ysa <isabelle.yim@gmail.com>
2018-05-01 18:24:52 +0200

    mainpage as waiting page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,25 @@
+import { Fragment } from 'react'
+import styled from 'styled-components'
+import Layout from '../components/Layout'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import { TitleH1 } from '../themes/global-styles'
+
+const Title = TitleH1.extend`
+  color: #726e75;
+  padding-top: 40vh;
+  text-transform: uppercase;
+  text-align: center;
+  height: 50vh;
+`
+
 const Index = () => (
-  <div>
-    <p>Coming soon</p>
-  </div>
+  <Fragment>
+    <Layout>
+      <Title>Coming soon</Title>
+      <Footer />
+    </Layout>
+  </Fragment>
 )
 
 export default Index
---
+++ themes/global-styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+//font-family: 'Gothic A1', sans-serif;
+//font-family: 'Noto Sans', sans-serif;
+//font-family: 'Shrikhand', cursive;
+
+// HEADINGS
+
+export const TitleH1 = styled.h1`
+  font-family: 'Shrikhand', cursive;
+  font-size: 48px;
+  letter-spacing: 2px;
+`
