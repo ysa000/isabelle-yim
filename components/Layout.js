4aa98b9cb1e8ad292937ab534954498f8bde42a1
Ysa <isabelle.yim@gmail.com>
2018-09-09 17:28:23 +0200

    Create new layout + set media queries

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,19 @@
 import styled from 'styled-components'
+import ScreenSize from '../styles/MediaQueries'
 
 const Layout = styled.main`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  border: 1px solid black;
+  margin: 0 auto;
+  width: 80%;
+  @media ${ScreenSize.tablet} {
+    width: 70%;
+  }
+  @media ${ScreenSize.desktop} {
+    width: 40%;
+  }
 `
 
 export default Layout
---
+++ styles/MediaQueries.js
@@ -0,0 +1,8 @@
+const ScreenSize = {
+  mobilePortrait: '(max-width: 640px)',
+  mobileLandscape: '(min-width: 640px) and (max-width: 768px)',
+  tablet: '(min-width: 768px) and (max-width: 1024px)',
+  desktop: '(min-width: 1024px)',
+}
+
+export default ScreenSize
