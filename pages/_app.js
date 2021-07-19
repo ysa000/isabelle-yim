5c0b08bd561d576cabf470e6a392183dcaaba11a
Ysa <isabelle.yim@gmail.com>
2021-07-19 21:39:16 +0200

    fix icons size

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,4 @@
 import { GlobalStyle } from '../styles/global-styles'
-import { config } from '@fortawesome/fontawesome-svg-core'
-config.autoAddCss = false
 
 export default function App({ Component, pageProps }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,10 @@ export default function Home() {
       <Head>
         <title>Isabelle Yim</title>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://use.fontawesome.com/releases/v5.15.3/css/svg-with-js.css"
+          rel="stylesheet"
+        ></link>
         <script
           src="https://kit.fontawesome.com/fa3635a591.js"
           crossOrigin="anonymous"
