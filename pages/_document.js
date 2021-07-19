ef2ac59f0259752c6b13d04542addcb809749180
Ysa <isabelle.yim@gmail.com>
2021-07-19 18:14:25 +0200

    new

--- pages/_document.js
+++ pages/_document.js
@@ -1,36 +1,30 @@
-import Document, { Head, Main, NextScript } from 'next/document'
+import Document from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
-    const page = renderPage(App => props =>
-      sheet.collectStyles(<App {...props} />),
-    )
-    const styleTags = sheet.getStyleElement()
-    return { ...page, styleTags }
-  }
+    const originalRenderPage = ctx.renderPage
 
-  render() {
-    return (
-      <html>
-        <Head>
-          <link
-            href="https://fonts.googleapis.com/css?family=Gothic+A1:300,400|Noto+Sans:400,400i|Nova+Mono|Open+Sans:300,400,600,700|Roboto"
-            rel="stylesheet"
-          />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          {this.props.styleTags}
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-          <script async src="https://ping.achkouyan.com/hi.js" />
-          <noscript>
-            <img src="https://ping.achkouyan.com/ping.gif" alt="" />
-          </noscript>
-        </body>
-      </html>
-    )
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,22 @@
-import React from 'react'
-import styled from 'styled-components'
-import { color } from '../styles/GlobalStyles'
-import Screensize from '../styles/MediaQueries'
-import Layout from '../components/Layout'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
+import Head from 'next/head'
+import Profile from '../components/Profile'
+import About from '../components/About'
+import Footer from '../components/Footer'
 
-library.add(fab)
-
-const ProfileContent = styled.main`
-  text-align: center;
-  padding: 30px 0;
-  color: ${color.outerSpace};
-  font-family: 'Gothic A1', sans-serif;
-  letter-spacing: 0.25em;
-  @media (min-width: 768px) {
-    min-width: auto;
-  }
-`
-
-const ProfilePicture = styled.img`
-  border-radius: 2px;
-  max-width: 150px;
-  height: auto;
-  margin-bottom: 40px;
-  ${Screensize.desktop} {
-    max-width: 250px;
-  }
-`
-
-const ProfileName = styled.h1`
-  font-size: 1.5em;
-  letter-spacing: 7px;
-  text-transform: uppercase;
-  padding-bottom: 30px;
-`
-
-const ProfileTitle = styled.h2`
-  font-size: 0.8em;
-  text-transform: uppercase;
-`
-
-const Index = () => (
-  <ProfileContent>
-    <ProfilePicture
-      src="/static/IMG_1172.jpg"
-      alt="Isabelle Yim profile picture"
-    />
-    <ProfileName>Isabelle Yim</ProfileName>
-    <ProfileTitle>Front End Developer</ProfileTitle>
-  </ProfileContent>
-)
-
-export default Index
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Isabelle Yim</title>
+        <link rel="icon" href="/favicon.ico" />
+        <script
+          src="https://kit.fontawesome.com/fa3635a591.js"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      <Profile src="/profile-picture.jpg" alt="Isabelle Yim" marginTop={30} />
+      <About />
+      <Footer />
+    </>
+  )
+}
